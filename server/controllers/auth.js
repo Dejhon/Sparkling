f94c7bccf76093d98d5343c8f3909b26547088b5
Dejhon <dejhonsherman6@gmail.com>
2022-08-06 13:51:24 -0500

    changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -10,11 +10,11 @@ exports.login = async (req, res) => {
     const { userName, password} = req.body;
     const user = await User.findOne({userName});
     if(!user){
-      return res.json({status:"Error", message:"User not found"})
+      return res.status(404).json({status:"Error", message:"User not found"})
     }
     const comparePassword = await bcrypt.compare(password, user.password);
     if(comparePassword){
-      res.send({status:"Successful", message:"Logged in successfully"});
+      res.status(201).json({status:"Successful", message:"Logged in successfully"});
       const token = jwt.sign({
         id: user._id,
         userName: user.userName
@@ -22,60 +22,11 @@ exports.login = async (req, res) => {
       JWT_SECRET,{
         expiresIn:86400
       })
-      return res.json({user, token: token})
+      return res.status(200).json({user, token: token})
     }else{
-      return res.json({status:"error", message:"Invalid Credentials"})
+      return res.status(400).json({status:"error", message:"Invalid Credentials"})
     }
   }catch(err){
-    res.status(400).json({
-      message:"Error Occured"
-    })
+   console.log(err);
   }
 }
-
-// exports.login =  async (req, res) =>{
-//   try{
-//   const user = User.findOne({userName:req.body.userName})
-//   if(!user){
-//     return res.status(404).json({
-//       message: "That user is not registered"
-//     })
-//   }
-//   if(!await bycrpt.compare(req.body.password, user.password)){
-//     return res.status(400).json({
-//       message: "That password is incorrect"
-//     })
-//   }
-
-//   res.status(201).json({
-//     message:"Logged In Successfully"
-//   })
-//   }catch(err){
-//     res.status(400).json({
-//       message:"User not found"
-//     })
-//   }
-// }
-
-// exports.login = async (req, res) => {
-//   try{
-//     const salt = await bycrpt.genSalt(10);
-//     const hashedPassword = await bycrpt.hash(req.body.password, salt)
-//     const user = await User.findOne({userName:req.body.userName, password:hashedPassword});
-//     if(!user){
-//       res.status(404).json({
-//         message:"Invalid Credential"
-//       })
-//     }else{
-//       res.status(200).json({
-//         message:"Login Successful",
-//         user: user
-//       })
-//     }
-//   }catch(err){
-//     res.status(404).json({
-//       status:"Invalind Credentials",
-//       message: err
-//    });
-//   } 
-// }
